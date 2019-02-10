d8b20901e884dd6e9574b315b47892e3ac7de3b8
James Wagstaff <wagstaff.james@gmail.com>
2019-02-10 20:25:04 +0000

    tidied up and documented

--- R/starr.R
+++ R/starr.R
@@ -1,5 +1,15 @@
+# starr.R - functions for ringo package
 
-
+#' Single Table to Tibble
+#'
+#' Turns a single table (as a vector of lines) from the STAR file into a tibble.
+#'
+#' @param z A Vector of all the lines that make one STAR table
+#'
+#' @return A tibble
+#'
+#' @examples
+#' single_table_to_df()
 single_table_to_df <- function(z){
   if (stringr::str_detect(z[[2]], pattern = "^loop_")) {
     if (length(z) < 3) {return(NA)} # Some lists are of 0 length...
@@ -10,7 +20,7 @@ single_table_to_df <- function(z){
                             delim = " ",
                             trim_ws = T,
                             col_names = names_cols)
-  } else {
+  } else { # For tables without a "loop_"
     z <- z[-1]
     df <- dplyr::tibble("data" = z)
     df <- tidyr::separate(df, data, into = c("key", "value"), sep = "[:blank:]+")
@@ -21,6 +31,19 @@ single_table_to_df <- function(z){
 }
 
 
+#' starr
+#'
+#' The main function in the package, for reading in a STAR file and loading
+#'  into memory as a list of tibbles.
+#'
+#' @param path_to_star The path to the STAR file.
+#'
+#' @return A list of tibbles, one for each of the tables in the STAR file.
+#'
+#' @examples
+#' ringo::starr("./model.star")
+#'
+#' @export
 starr <- function(path_to_star){
   x <- readr::read_lines(path_to_star)
   x <- trimws(x, which = "both")
